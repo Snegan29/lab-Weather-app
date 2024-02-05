54e1b39e170a9c0a6b2b68183183780fc03a7364
Snegan P <pssj2914@gmail.com>
2024-02-05 08:24:42 +0530

    Created weather app and it is fully functional

---
+++ weather.js
@@ -0,0 +1,60 @@
+
+
+let form = document.getElementById('input');
+let cityName = document.getElementById("input-city");
+let APIkey = '14a64bd41a53747e067e994861bda5c6';
+
+
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    getCityData();
+});
+
+function getCityData() {
+
+    fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${cityName.value}&limit=5&appid=${APIkey}`)
+    .then((response) =>response.json())
+    .then((data) => {
+        console.log("City-Data:",data)
+        getCoordinates(data)
+    })
+    .catch((error) => console.log(error))
+}
+
+
+function getCoordinates(apiData) {
+    let latitude = apiData[0].lat
+    let longitude = apiData[0].lon
+
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${APIkey}`)
+    .then((response) => response.json())
+    .then((apiData) => {
+        console.log("Co-ordinates:",apiData)
+        appendData(apiData,cityName)
+    })
+    .catch((error) => console.log(error))
+    
+}
+
+
+function appendData(apiData,cityName) {
+    
+    const options = { weekday: 'long', day: 'numeric', month: 'long', year: 'numeric' };
+    const formattedDate = new Date().toLocaleDateString('en-US', options);
+    
+    let city = document.getElementById("city-name")
+    let state = document.getElementById("state-name")
+    let date = document.getElementById("date")
+    let temperature = document.getElementById("temp")
+    let weather = document.getElementById("weather")
+    let fullTemp = document.getElementById("fulltemp")
+    
+    city.innerText = `${cityName.value},${apiData.sys.country}`
+    state.innerText = `${apiData.sys.state}`
+    temperature.innerText = (apiData.main.temp - 273.15).toFixed(1) + " °C";
+    weather.innerText = apiData.weather[0].description;
+    date.innerText = formattedDate;
+    fullTemp.innerText = `${(apiData.main.temp_min-273.15).toFixed(1)+ " °C"}/${(data.main.temp_max-273.15).toFixed(1)+ " °C"}`
+
+}
